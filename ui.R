1fe545992309923774a44ff3598f812198032cce
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-01 13:32:37 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -14,7 +14,7 @@ ui <- f7Page(
       strong = TRUE,
       inset = TRUE,
       div(
-        style = "border: 1px solid #ccc; border-radius: 8px; padding: 5px; background-color: #fff;",
+        style = "border: 1px solid #ccc; border-radius: 8px; padding: 2px; background-color: #fff;",
         f7Text(
           inputId = "search_sku",
           label = NULL,
